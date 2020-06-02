608c0b8546742e519022100ac46929ccd968314c
Megan_Comeau <mlcomeau99@gmail.com>
2020-06-02 11:27:24 -0700

    added application controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,27 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do 
+    if logged_in?
+      redirect "/tasks"
+    end
     erb :home 
   end 
+
+  helpers do 
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect '/'
+      end
+    end
+
+  end
 
 end
