25d71e6cad35d4f16f42730b62b97b8fb1c1f42e
Megan_Comeau <mlcomeau99@gmail.com>
2020-06-01 22:49:13 -0700

    initial commit app skeleton db migrated

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+require './config/environment'
+require 'rack-flash'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions 
+    set :session_secret, "tooty-fruity"
+    use Rack::Flash 
+  end
+
+  get '/' do 
+    erb :home 
+  end 
+
+end
---
+++ config.ru
@@ -0,0 +1,7 @@
+require_relative "./config/environment"
+
+
+use Rack::MethodOverride
+#use UsersController
+#use TasksController
+run ApplicationController
